1e5c5bcb9716ccb25e26effced3695346d8cdccc
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-11-11 21:43:42 +0900

    20241111 chapter03

--- spring-boot-example/chapter03/src/main/java/com/springtour/example/chapter03/SpringBean07Application.java
+++ spring-boot-example/chapter03/src/main/java/com/springtour/example/chapter03/SpringBean07Application.java
@@ -16,13 +16,14 @@ public class SpringBean07Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctxt = SpringApplication.run(SpringBean07Application.class);
-        PriceUnit priceUnit = ctxt.getBean(PriceUnit.class);
+        PriceUnit priceUnit = ctxt.getBean(PriceUnit.class); // 같은 클래스 타입에 대한 의존성이 여러개 일 경우 NoUniqueBeanDefinitionException 발생함
         log.info("Locale in PriceUnit : {}", priceUnit.getLocale().toString());
         ctxt.close();
     }
 
+
     @Bean
-//    @Primary
+    @Primary // 해당 애너테이션을 설정함으로써 primaryPriceUnit 스프링 빈이 주입됨
     public PriceUnit primaryPriceUnit() {
         return new PriceUnit(Locale.US);
     }
--- spring-boot-example/chapter03/src/main/java/com/springtour/example/chapter03/SpringBean09Application.java
+++ spring-boot-example/chapter03/src/main/java/com/springtour/example/chapter03/SpringBean09Application.java
@@ -17,7 +17,9 @@ public class SpringBean09Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctxt = SpringApplication.run(SpringBean09Application.class);
         log.info("------- Done to initialize spring beans");
+        // @Lazy를 사용하였기 때문에 위에 context가 로드되었지만 "initialize lazyPriceUnit" 로그가 출력되지 않았음
         PriceUnit priceUnit = ctxt.getBean("lazyPriceUnit",PriceUnit.class);
+        // getBean 메서드를 사용함으로 써 lazyPriceUnit 스프링 빈을 생성하여 "initialize lazyPriceUnit" 로그가 출력됨
         log.info("Locale in PriceUnit : {}", priceUnit.getLocale().toString());
         ctxt.close();
     }
